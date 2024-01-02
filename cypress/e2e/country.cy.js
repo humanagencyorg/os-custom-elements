14caba3526b367358579c9a69f69075755d6abbd
Yehor Oliinyk <egorslam@users.noreply.github.com>
2024-01-02 15:28:14 -0800

    WIP: Does not call API if country tag does not have data-field-uuid

--- cypress/e2e/country.cy.js
+++ cypress/e2e/country.cy.js
@@ -1,23 +1,51 @@
-context("country field", function () {
+context("country field", function() {
   beforeEach(() => {
     cy.fixture("countries_response.json").as("countriesResponse");
   });
 
-  it("passes workspace id from the script src to the request headers", function () {
-      cy.intercept("GET", "**/api/v1/data_fields/country_field_uuid/countries", this.countriesResponse)
-        .as("countriesSuccess");
-      cy.visit("/");
+  it("passes workspace id from the script src to the request headers", function() {
+    cy.intercept("GET", "**/api/v1/data_fields/country_field_uuid/countries", this.countriesResponse)
+      .as("countriesSuccess");
+    cy.visit("/");
 
     cy.wait("@countriesSuccess").then(
       (interception) => {
-        console.log(interception)
         expect(interception.request.headers["workspace-id"]).to.equal("1");
       },
     );
   });
 
+
+  describe("when element tag missing 'data-os-uuid' tag", () => {
+    it("warns user in console", function() {
+      cy.visit("/", {
+        onBeforeLoad(win) {
+          cy.stub(win.console, "warn").as("consoleWarn");
+        },
+      });
+
+      // NOTE: Need to remove 'data-os-uuid' from <os-county> tag.
+
+      cy.get("@consoleWarn").should(
+        "be.calledWith",
+        "data-os-uuid was not found for <os-county> element",
+      );
+    });
+
+    it("does not make API request with null uuid", function() {
+      cy.intercept("GET", "**/api/v1/data_fields/**/countries").as("countriesRequest");
+
+      // NOTE: Need to remove 'data-os-uuid' from <os-county> tag.
+
+      cy.visit("/");
+      cy.wait(500);
+
+      cy.get("@countriesRequest.all").should("have.length", 0);
+    });
+  })
+
   describe("when countries request succeeded", () => {
-    it("populates select options", function () {
+    it("populates select options", function() {
       cy.intercept("GET", "**/api/v1/data_fields/country_field_uuid/countries", this.countriesResponse)
         .as("countriesSuccess");
       cy.visit("/");
@@ -29,7 +57,7 @@ context("country field", function () {
       });
     });
 
-    it("sets default value from the attribute", function () {
+    it("sets default value from the attribute", function() {
       cy.intercept("GET", "**/api/v1/data_fields/country_field_uuid/countries", this.countriesResponse)
         .as("countriesSuccess");
       cy.visit("/");
@@ -39,7 +67,7 @@ context("country field", function () {
       });
     });
 
-    it("sets custom tag value on country select", function () {
+    it("sets custom tag value on country select", function() {
       cy.intercept("GET", "**/api/v1/data_fields/country_field_uuid/countries", this.countriesResponse)
         .as("countriesSuccess");
       cy.visit("/");
--- src/components/country.js
+++ src/components/country.js
@@ -5,48 +5,57 @@ export class OSCountry extends HTMLElement {
     super();
   }
 
+
   connectedCallback() {
     const defaultValue = this.getAttribute("value");
     const dataFieldUuid = this.getAttribute("data-os-uuid")
     const selectEl = document.createElement("select");
     this.appendChild(selectEl);
 
-    const emptyOption = document.createElement("option");
-    emptyOption.textContent = "";
-    emptyOption.value = "";
+    console.log('script');
+    const emptyOption = this.createOption("", "")
     selectEl.appendChild(emptyOption);
 
     const requestHost = host || "https://app.formli.com";
 
-    fetch(`${requestHost}/api/v1/data_fields/${dataFieldUuid}/countries`, {
-      headers: {
-        "Workspace-Id": workspaceId,
-      },
-    })
-      .then(async (response) => {
-        if (response.ok) {
-          const countries = await response.json();
-
-          countries.data.forEach((country) => {
-            const option = document.createElement("option");
-            option.textContent = country.name;
-            option.value = country.code;
-            selectEl.appendChild(option);
-          });
-
-          if (defaultValue) {
-            selectEl.value = defaultValue;
-          }
-        } else {
-          console.error("Something went wrong. Try again later.");
-        }
+    if (dataFieldUuid) {
+      fetch(`${requestHost}/api/v1/data_fields/${dataFieldUuid}/countries`, {
+        headers: {
+          "Workspace-Id": workspaceId,
+        },
       })
-      .catch((error) => {
-        console.error(error);
-      });
+        .then(async (response) => {
+          if (response.ok) {
+            const countries = await response.json();
+
+            countries.data.forEach((country) => {
+              const option = this.createOption(country.name, country.code);
+              selectEl.appendChild(option);
+            });
+
+            if (defaultValue) {
+              selectEl.value = defaultValue;
+            }
+          } else {
+            console.error("Something went wrong. Try again later.");
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      console.warn("data-os-uuid was not found for <os-county> element")
+    }
 
     selectEl.addEventListener("change", (event) => {
       this.setAttribute("value", event.target.value);
     });
+  }
+
+  createOption(text, value) {
+    const option = document.createElement("option");
+    option.textContent = text;
+    option.value = value;
+    return option;
   }
 }
