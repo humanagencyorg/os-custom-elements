ae7a22d7d5ad15538a3db763bda254f9c31f25e3
Vitalii Shytikov <v.shytikov@gmail.com>
2024-01-22 12:52:51 +0100

    move dispatch to functions

--- src/components/file_upload.js
+++ src/components/file_upload.js
@@ -24,14 +24,7 @@ export class OSFileUpload extends HTMLElement {
       const signedId = blob?.signed_id;
 
       if (error) {
-        this.dispatchEvent(
-          new CustomEvent("upload-error", { detail: { error } }),
-        );
-        // Abort all ongoing uploads
-        this.uploaders.forEach((uploader) => uploader.abort());
-        this.uploaders = [];
-        this.completedUploadIds = [];
-
+        this.dispatchErrorEvent(error);
         uploadReset();
       } else {
         const signedIdInput = document.createElement("input");
@@ -41,9 +34,11 @@ export class OSFileUpload extends HTMLElement {
 
         this.appendChild(signedIdInput);
         // Remove the completed uploader from the list
-        this.uploaders = this.uploaders.filter((u) => u.file.name !== blob.filename);
+        this.uploaders = this.uploaders.filter((u) =>
+          u.file.name !== blob.filename
+        );
         if (this.uploadCounter === this.totalFiles) {
-          this.dispatchEvent(new CustomEvent("upload-success", { detail: { completedUploadIds: this.completedUploadIds }}));
+          this.dispatchSuccessEvent(this.completedUploadIds);
           this.uploadCounter = 0;
         }
       }
@@ -60,14 +55,18 @@ export class OSFileUpload extends HTMLElement {
       this.uploaders = [];
       this.completedUploadIds = [];
 
-      if (filesLimit && files.length > Number(filesLimit)) {
-        const text =
-          `Limit of ${filesLimit} files`;
-        this.dispatchEvent(
-          new CustomEvent("upload-error", { detail: { error: text } }),
-        );
+      if (this.filesExceedLimit(files, filesLimit)) {
+        const text = `Limit of ${filesLimit} files`;
+
+        this.dispatchErrorEvent(text);
         uploadReset();
-      } else if (this.filesHaveCorrectSize(files)) {
+      } else if (this.fileExceedsMaxSize(files)) {
+        const text =
+          "File size exceeds the limit of 25MB. Please select a smaller file.";
+
+        this.dispatchErrorEvent(text);
+        uploadReset();
+      } else {
         files.forEach((file) => {
           const requestHost = host || "https://app.formli.com";
           const uploader = new Uploader(
@@ -79,22 +78,33 @@ export class OSFileUpload extends HTMLElement {
           this.uploaders.push(uploader);
           uploader.start();
         });
-      } else {
-        const text =
-          "File size exceeds the limit of 25MB. Please select a smaller file.";
-        this.dispatchEvent(
-          new CustomEvent("upload-error", { detail: { error: text } }),
-        );
-        uploadReset();
       }
     });
 
     const uploadReset = () => {
       this.removeSignedIdInputs();
       fileInput.value = "";
+      // Abort all ongoing uploads
+      this.uploaders.forEach((uploader) => uploader.abort());
+      this.uploaders = [];
+      this.completedUploadIds = [];
     };
 
     this.addEventListener("upload-reset", uploadReset);
+  }
+
+  dispatchErrorEvent(text) {
+    this.dispatchEvent(
+      new CustomEvent("upload-error", { detail: { error: text } }),
+    );
+  }
+
+  dispatchSuccessEvent(completedUploadIds) {
+    this.dispatchEvent(
+      new CustomEvent("upload-success", {
+        detail: { completedUploadIds: completedUploadIds },
+      }),
+    );
   }
 
   removeSignedIdInputs() {
@@ -104,8 +114,12 @@ export class OSFileUpload extends HTMLElement {
     });
   }
 
-  filesHaveCorrectSize(files) {
+  filesExceedLimit(files, filesLimit) {
+    return filesLimit && files.length > Number(filesLimit);
+  }
+
+  fileExceedsMaxSize(files) {
     const maxSizeInBytes = 25 * 1024 * 1024; // 25MB in bytes
-    return files.every((file) => file.size <= maxSizeInBytes);
+    return files.some((file) => file.size > maxSizeInBytes);
   }
 }
