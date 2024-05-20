d2d9a414c6e35ea8c8dd8c72ec9ae54d5cf947a9
Vitalii Shytikov <v.shytikov@gmail.com>
2024-05-20 12:09:15 +0200

    set column names into table header

--- src/components/table.js
+++ src/components/table.js
@@ -11,51 +11,73 @@ export class OSTable extends HTMLElement {
 
     if (haveLoopAttribute) {
       const responseViewUuid = this.getAttribute("data-os-view");
-      fetch(
-        `${requestHost}/api/v1/response_views/${responseViewUuid}/items`,
-        {
-          credentials: "include",
-          headers: {
-            "Workspace-Id": workspaceId,
+      if (!responseViewUuid) {
+        fetch(
+          `${requestHost}/api/v1/response_views/${responseViewUuid}/items`,
+          {
+            credentials: "include",
+            headers: {
+              "Workspace-Id": workspaceId,
+            },
           },
-        },
-      ).then(async (response) => {
-        if (response.ok) {
-          const { data } = await response.json();
-          if (data.length > 0) {
-            const table = document.createElement("table");
-            const thead = document.createElement("thead");
-            const tbody = document.createElement("tbody");
-            table.appendChild(thead);
-            table.appendChild(tbody);
-            // hide the empty element
-            this.dispatchEvent(
-              new CustomEvent("table-success", { detail: { element: this } }),
-            );
+        ).then(async (response) => {
+          if (response.ok) {
+            const { data } = await response.json();
+            if (data.length > 0) {
+              const table = document.createElement("table");
+              const thead = document.createElement("thead");
+              const tbody = document.createElement("tbody");
+              table.appendChild(thead);
+              table.appendChild(tbody);
+              // hide the empty element
+              this.dispatchEvent(
+                new CustomEvent("table-success", { detail: { element: this } }),
+              );
 
-            data.forEach((columns) => {
-              const tableRow = document.createElement("tr");
-              columns.forEach((column) => {
-                const td = document.createElement("td");
-                td.innerHTML = column.value;
-                td.setAttribute("data-os-default", "column");
-                td.setAttribute("data-os-column", column.uuid);
-                tableRow.appendChild(td);
+              data.forEach((columns, index) => {
+                const tbodyRow = document.createElement("tr");
+                const theadRow = document.createElement("tr");
+                columns.forEach((column) => {
+                  if (index === 0) {
+                    // create thead
+                    const th = document.createElement("th");
+                    th.innerHTML = column.name;
+                    theadRow.appendChild(th);
+                    thead.appendChild(theadRow);
+                  }
+                  // create tbody
+                  const td = document.createElement("td");
+                  td.innerHTML = column.value;
+                  td.setAttribute("data-os-default", "column");
+                  td.setAttribute("data-os-column", column.uuid);
+                  tbodyRow.appendChild(td);
+                });
+                tbody.appendChild(tbodyRow);
               });
-              tbody.appendChild(tableRow);
-            });
 
-            this.appendChild(table);
-          } else {
-            // show the empty element
+              this.appendChild(table);
+            } else {
+              // show the empty element
+              this.dispatchEvent(
+                new CustomEvent("table-empty", { detail: { element: this } }),
+              );
+            }
           }
-        }
-      }).catch((error) => {
-        console.error(error);
-        this.dispatchEvent(
-          new CustomEvent("table-error", { detail: { error } }),
+        }).catch((error) => {
+          console.error(error);
+          this.dispatchEvent(
+            new CustomEvent("table-error", { detail: { error } }),
+          );
+        });
+      } else {
+        console.warn(
+          'data-os-view is not set for <os-table> element',
         );
-      });
+      }
+    } else {
+      console.warn(
+        'data-os-element="loop" attribute was not found for <os-table> element',
+      );
     }
   }
 }
